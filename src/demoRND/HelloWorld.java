966109249d0347685303ef45fdb22359a08b78e0
omkar <omkar.shripat@mobicule.com>
2025-06-18 12:57:53 +0530

    changr

--- src/demoRND/HelloWorld.java
+++ src/demoRND/HelloWorld.java
@@ -3,6 +3,6 @@ package demoRND;
 //HelloWorld.java
 public class HelloWorld {
  public static void main(String[] args) {
-     System.out.println("Hello, World!");
+     System.out.println("Hello, Worlds!");
  }
 }
